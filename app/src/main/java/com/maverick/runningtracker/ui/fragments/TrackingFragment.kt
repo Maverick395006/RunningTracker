557e4a644ca9c98b62c3433668f462b1439d24aa
Piyush <piyush.radadiya288@gmail.com>
2024-01-03 10:48:12 +0530

    Tracking Service Basic Setup

--- app/src/main/java/com/maverick/runningtracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/maverick/runningtracker/ui/fragments/TrackingFragment.kt
@@ -1,13 +1,17 @@
 package com.maverick.runningtracker.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
 import com.maverick.runningtracker.R
+import com.maverick.runningtracker.services.TrackingService
 import com.maverick.runningtracker.ui.viewmodels.MainViewModel
+import com.maverick.runningtracker.util.Constants.ACTION_START_OR_RESUME_SERVICE
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.btnToggleRun
 import kotlinx.android.synthetic.main.fragment_tracking.mapView
 
 @AndroidEntryPoint
@@ -21,10 +25,21 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         super.onViewCreated(view, savedInstanceState)
         mapView.onCreate(savedInstanceState)
 
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+
         mapView.getMapAsync {
             map = it
         }
 
+    }
+
+    private fun sendCommandToService(action:String) {
+        Intent(requireContext(),TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
     }
 
     override fun onResume() {
