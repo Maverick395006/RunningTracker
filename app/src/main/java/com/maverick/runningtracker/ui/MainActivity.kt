d806aeadf1ece21d5f83ce46cc4c788ec96341b0
Piyush <piyush.radadiya288@gmail.com>
2024-01-04 15:16:41 +0530

    Making Our Service a Foreground Service
    - Create Notification Channel
    - Create Constants
    - Notification Channel
    - Notification Manager
    - Notification Builder
    - Pending Intent
    - Global Action
    - Tracking Fragment
    - Sending Pending Intent
    - Navigate to Fragment

--- app/src/main/java/com/maverick/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/com/maverick/runningtracker/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.maverick.runningtracker.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.maverick.runningtracker.R
+import com.maverick.runningtracker.util.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.bottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.navHostFragment
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
@@ -31,6 +35,17 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 
 }
