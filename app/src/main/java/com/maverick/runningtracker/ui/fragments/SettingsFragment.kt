1516d95af1633a24b09eb2d652f3ef28148717e1
Piyush <piyush.radadiya288@gmail.com>
2024-01-11 18:15:37 +0530

    Total Run Statistics & SettingsFragment

--- app/src/main/java/com/maverick/runningtracker/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/maverick/runningtracker/ui/fragments/SettingsFragment.kt
@@ -1,7 +1,65 @@
 package com.maverick.runningtracker.ui.fragments
 
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.maverick.runningtracker.R
+import com.maverick.runningtracker.util.Constants.KEY_FIRST_TIME_TOGGLE
+import com.maverick.runningtracker.util.Constants.KEY_NAME
+import com.maverick.runningtracker.util.Constants.KEY_WEIGHT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.tvToolbarTitle
+import kotlinx.android.synthetic.main.fragment_settings.btnApplyChanges
+import kotlinx.android.synthetic.main.fragment_settings.etName
+import kotlinx.android.synthetic.main.fragment_settings.etWeight
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadFieldsFromSharedPref()
+
+        btnApplyChanges.setOnClickListener {
+            val success = applyChangesToSharedPref()
+            if (success) {
+                Snackbar.make(view, "Saved changes", Snackbar.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(view, "Please fill out all the fields", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+    }
+
+    private fun loadFieldsFromSharedPref() {
+        val name = sharedPreferences.getString(KEY_NAME, "")
+        val weight = sharedPreferences.getFloat(KEY_WEIGHT, 80f)
+        etName.setText(name)
+        etWeight.setText(weight.toString())
+    }
+
+    private fun applyChangesToSharedPref(): Boolean {
+        val name = etName.text.toString()
+        val weight = etWeight.text.toString()
+        if (name.isEmpty() || weight.isEmpty()) {
+            return false
+        }
+        sharedPreferences.edit()
+            .putString(KEY_NAME, name)
+            .putFloat(KEY_WEIGHT, weight.toFloat())
+            .putBoolean(KEY_FIRST_TIME_TOGGLE, false)
+            .apply()
+        val toolbarText = "Let's go, $name"
+        requireActivity().tvToolbarTitle.text = toolbarText
+        return true
+    }
+
 }
--- app/src/main/java/com/maverick/runningtracker/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/maverick/runningtracker/ui/viewmodels/StatisticsViewModel.kt
@@ -7,4 +7,12 @@ import com.maverick.runningtracker.repositories.MainRepository
 class StatisticsViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ) : ViewModel() {
+
+    val totalTimeRun = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistanceInMeters()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+    val totalAvgSpeed = mainRepository.getTotalAvgSpeedInKMH()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+
 }
